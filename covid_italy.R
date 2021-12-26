dc7fd3f022a54728017433a33103fe71670ed8d3
ale-ch <alessiochiodin@hotmail.com>
2021-12-26 17:08:19 +0100

    Delete covid_italy.R

--- covid_italy.R
+++
@@ -1,8 +0,0 @@
-source("https://raw.githubusercontent.com/ale-ch/it_covid_dashboard/main/dataset_nazionale.R")
-source("https://raw.githubusercontent.com/ale-ch/it_covid_dashboard/main/dataset_regionale.R")
-
-shared_names <- names(df_regioni)[which(names(df_regioni) %in% names(df))]
-
-covid_italy <- full_join(df, df_regioni, by = shared_names)
-rm(df)
-rm(df_regioni)
