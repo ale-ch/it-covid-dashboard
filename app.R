31be4224d54a38c7ae9249e99d53e71b2d5aa748
ale-ch <alessiochiodin@hotmail.com>
2021-12-26 20:08:36 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,11 +1,15 @@
 library(shiny)
 
-source("https://raw.githubusercontent.com/ale-ch/it_covid_dashboard/main/dataset_create.R")
-source("https://raw.githubusercontent.com/ale-ch/it_covid_dashboard/main/covid_plot.R")
+source("https://raw.githubusercontent.com/ale-ch/it_covid_dashboard/main/dataset_create.R") # read in source code for dataset
+source("https://raw.githubusercontent.com/ale-ch/it_covid_dashboard/main/covid_plot.R") # read in source code for helper function
 
+# list of italy's regions 
 regions <- c(NULL, levels(as.factor(covid_italy$denominazione_regione)))
+
+# max number of days of data available
 max_days <- length(seq(min(covid_italy$data), max(covid_italy$data), by = "days"))
 
+# selected variables for visualization
 selected_names <- names(covid_italy) %in% c("data", "stato", "denominazione_regione", 
                                            "codice_regione", "lat","long")
 selected_names <- names(covid_italy)[selected_names == FALSE]
@@ -20,13 +24,14 @@ ui <- fluidPage(
                         min = 1, max = max_days)
                  ),
           column(6,
-            selectInput("variable", "Select variable to visualize:", selected_names,
-                        selected = "Nazionale")
+            selectInput("variable", "Select variable to visualize:", 
+                        choices = selected_names)
           )
         ),
 
         checkboxGroupInput("region", "Select region(s):", regions,
-                           inline = TRUE),
+                           inline = TRUE,
+                           selected = "Nazionale"),
         
         fluidRow(
           column(4, 
@@ -36,20 +41,16 @@ ui <- fluidPage(
             radioButtons("percent", "Visualize in percentages:", c(FALSE, TRUE))
           )
         ),
-        
-        fluidRow(
-          column(8, 
-            plotOutput("plot", width = "100%")
-          ),
-          column(4,
-            verbatimTextOutput("text1")
-          )
-        )
+
+        plotOutput("plot", width = "100%"),
+        dataTableOutput("static")
+ 
     )
 
 
 server <- function(input, output, session) {
-
+    
+  # plot using covid_plot() function
     output$plot <- renderPlot({
         covid_plot(df = covid_italy,
                    var = input$variable, 
@@ -60,17 +61,21 @@ server <- function(input, output, session) {
                    )
     })
     
-    output$text1 <- renderPrint({
-      writeLines(
-        paste0("date: ", as.character(max(covid_italy$data)), "\n",
-               
-               "Rt: ", as.character(round(covid_italy$Rt[nrow(covid_italy)], 2)), "\n",
-               "new infections:" , as.character(covid_italy$nuovi_positivi[nrow(covid_italy)]), "\n",
-               "number of tests: ", as.character(covid_italy$variazione_tamponi[nrow(covid_italy)]), "\n",
-               "infections-tests ratio (%): ", 
-               as.character(round(covid_italy$ratio_positivi_tamponi[nrow(covid_italy)] * 100), 2), "%"
-               ))
-        
+  # output some statistics in a table
+    output$static <- renderDataTable({
+      
+      covid_italy %>% 
+        group_by(denominazione_regione) %>% 
+        filter(
+          data == max(data),
+          denominazione_regione %in% input$region) %>% 
+        summarise(
+          date = data,
+          Rt = round(Rt, 2),
+          new_infections = nuovi_positivi,
+          new_tests = nuovi_tamponi,
+          infections_tests_ratio = paste0(as.character(round(
+            ratio_positivi_tamponi * 100), 2),"%"))
     })
 }
 
